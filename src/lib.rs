8708bd1fb6a7bff3ca976553ddc5fcc46332db63
h0p3so <juan.dpmz05@gmail.com>
2026-06-03 22:54:37 -0500

    parsing shortflags

--- src/lib.rs
+++ src/lib.rs
@@ -6,25 +6,63 @@ pub enum Error {
     DupLongname(usize),
     AnonymousFlag(usize),
     InvalidShortname(usize),
+    ShouldntExpectedAValue(usize),
+    ShouldExpectedAValue(usize),
+
+    UnknownShortname(String, usize),
+    BadGrouping(String, usize)
+}
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum ArgMode {
+    Required,
+    Optional,
+    NoArgument
+}
+
+pub enum ArgValue {
+    Txt(String),
+    F64(   f64),
+    I32(   i32),
+    U32(   u32),
+    I64(   i64),
+    U64(   u64)
+}
+
+#[derive(Copy, Clone)]
+pub enum ArgExpectedType {
+    Txt,
+    F64,
+    I32,
+    U32,
+    I64,
+    U64
 }
 
 pub struct Flag {
     pub shortname: Option<char>,
-    pub longname: Option<&'static str>,
+    pub longname:  Option<&'static str>,
+    pub value:     Option<ArgValue>,
+    pub expected:  Option<ArgExpectedType>,
+
     pub seen: bool,
+    pub mode: ArgMode,
 }
 
 #[derive(Debug)]
 pub struct Argrs {
-    pub positional: Vec<String>,
+    pub flastindex: Option<usize>,
 }
 
 impl Flag {
     pub fn default () -> Self {
         Self {
             shortname: None,
-            longname: None,
-            seen: false,
+            longname : None,
+            value    : None,
+            expected : None,
+            seen     : false,
+            mode     : ArgMode::NoArgument
         }
     }
 }
@@ -34,6 +72,7 @@ impl Flag {
  * - has either a shortname or longname
  * - there are not flags with the same shortname or longname
  * - shortnames is valid (A-Za-z0-9)
+ * - all flags make sense in terms of argument taking
  */
 fn check_integrity (flags: &[Flag]) -> Result<(), Error> {
     let mut shortmapper: HashSet<char> = HashSet::new();
@@ -42,6 +81,16 @@ fn check_integrity (flags: &[Flag]) -> Result<(), Error> {
     for (i, flag) in flags.iter().enumerate() {
         if flag.shortname.is_none() && flag.longname.is_none() {
             return Err(Error::AnonymousFlag(i));
+        }
+
+        match (flag.mode, flag.expected) {
+            (ArgMode::NoArgument, Some(_expected)) => {
+                return Err(Error::ShouldntExpectedAValue(i));
+            }
+            (ArgMode::Required | ArgMode::Optional, None) => {
+                return Err(Error::ShouldExpectedAValue(i));
+            }
+            _ => {}
         }
 
         if let Some(shrtname) = flag.shortname {
@@ -63,8 +112,65 @@ fn check_integrity (flags: &[Flag]) -> Result<(), Error> {
     Ok(())
 }
 
+fn parse_shortopt (source: &String, flags: &mut [Flag]) -> Result<Option<usize>, Error> {
+    let mut lastseen: Option<usize> = None;
+    let srclen: usize = source.len();
+
+    let srcoffset: usize = 1;
+    for (i, shortname) in source.chars().skip(1).enumerate() {
+        match flags.iter().position(|f| f.shortname == Some(shortname)) {
+            Some(index) => {
+                lastseen = Some(index);
+                let flag: &mut Flag = &mut flags[index];
+
+                /* +1 since we're trying to know if there are more characters (shortnames) to
+                 * be parsed.
+                 *
+                 * srcoffset because we're skiping the '-' from the original `source` argument
+                 */
+                if flag.mode == ArgMode::Required && (i + 1 + srcoffset) < srclen {
+                    return Err(Error::BadGrouping(source.clone(), i + srcoffset))
+                }
+
+                flag.seen = true;
+            },
+            None => {
+                return Err(Error::UnknownShortname(source.clone(), i + srcoffset))
+            }
+        }
+    }
+
+    Ok(lastseen)
+}
+
 pub fn argrs (args: Vec<String>, flags: &mut [Flag]) -> Result<Argrs, Error> {
     check_integrity(flags) ?;
+    let mut f_lasidx: Option<usize> = None;
 
-    Ok(Argrs { positional: Vec::new() })
+    for arg in args.iter().skip(1) {
+        let length: usize = arg.len();
+        let char1 : Option<char> = arg.chars().nth(0);
+        let char2 : Option<char> = arg.chars().nth(1);
+
+        match (length, char1, char2) {
+            (2.., Some('-'), Some(ch2)) if ch2.is_ascii_alphanumeric() => {
+                f_lasidx = parse_shortopt(&arg, flags) ?;
+            }
+            (3.., Some('-'), Some('-')) => {
+                todo!();
+            }
+            (2, Some('-'), Some('-')) => {
+                todo!();
+            }
+            _ => {
+                todo!();
+            }
+        }
+    }
+
+    // TODO: make sure the prev flag has its argument
+
+    Ok(Argrs {
+        flastindex: f_lasidx,
+    })
 }
