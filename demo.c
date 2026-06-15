b9d532d70469b1510d6f21e883297843836c8add
h0p3so <juan.dpmz05@gmail.com>
2026-06-14 23:17:01 -0500

    good progress

--- demo.c
+++ demo.c
@@ -6,6 +6,7 @@
 int main () {
 	int *numbers = NULL;
 	int a = stdv_put(numbers, 5);
+	printf("is empty: %d\n", stdv_is_empty(numbers));
 
 	printf("%d\n", numbers[0]);
 	printf("%ld\n", stdv_len_u64(numbers));
@@ -22,6 +23,40 @@ int main () {
 	printf("poped: %d\n", a);
 	printf("%ld\n", stdv_len_u64(numbers));
 	printf("%ld\n", stdv_cap_u64(numbers));
+
+	puts("-*-");
+	stdv_put(numbers, -69);
+	printf("%d\n", stdv_get(numbers, 1));
+	printf("%ld\n", stdv_len_u64(numbers));
+	printf("%ld\n", stdv_cap_u64(numbers));
+
+
+	puts("-*-");
+	stdv_put(numbers, -9);
+
+	printf("beg: %d\n", stdv_beg(numbers));
+	printf("beg: %d\n", stdv_get(numbers, 1));
+	printf("end: %d\n", stdv_end(numbers));
+
+	puts("-*-");
+	a = stdv_pop(numbers);
+	a = stdv_pop(numbers);
+	a = stdv_pop(numbers);
+	printf("len: %ld\n", stdv_len_u64(numbers));
+	printf("cap: %ld\n", stdv_cap_u64(numbers));
+
+	puts("-----------------------------------------");
+
+	printf("is empty: %d\n", stdv_is_empty(numbers));
+	a = stdv_put(numbers, 1);
+	printf("is empty: %d\n", stdv_is_empty(numbers));
+	printf("%d\n", numbers[0]);
+	printf("len: %ld\n", stdv_len_u64(numbers));
+	printf("cap: %ld\n", stdv_cap_u64(numbers));
+	puts("shrinking");
+	stdv_shrink(numbers);
+	printf("len: %ld\n", stdv_len_u64(numbers));
+	printf("cap: %ld\n", stdv_cap_u64(numbers));
 
 	return 0;
 }
--- stdv.h
+++ stdv.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /*
  *  _____________________________________
@@ -18,23 +19,28 @@
 typedef struct {
 	uint64_t cap;
 	uint64_t len;
+	bool     is_mut;
 } _StdvHeader;
 
-#define _STDV_INIT_CAP             64
-#define _STDV_GROWTH_FACTOR        2
-#define _STDV_GET_STDVHEADER(vec) ((_StdvHeader*) (vec) - 1)
+#define _STDV_INIT_CAP                 64
+#define _STDV_GROWTH_FACTOR            2
+#define _STDV_GET_STDVHEADER(vec)      ((_StdvHeader*) (vec) - 1)
 
-void *_stdv_needs_to_grow (void *vec, size_t membsz) {
-	if (vec == NULL) {
+void *_stdv_needs_to_grow (void *vec, size_t membsz)
+{
+	if (vec == NULL)
+	{
 		_StdvHeader *header = (_StdvHeader*) malloc(_STDV_INIT_CAP * membsz + sizeof(_StdvHeader));
 		header->len         = 0;
 		header->cap         = _STDV_INIT_CAP;
+		header->is_mut      = true;
 		vec                 = (void*)(header + 1);
 		return vec;
 	}
 
-	_StdvHeader *header = ((_StdvHeader*) (vec) - 1);
-	if (header->len < header->cap) {
+	_StdvHeader *header = _STDV_GET_STDVHEADER(vec);
+	if (header->len < header->cap)
+	{
 		return vec;
 	}
 
@@ -44,6 +50,19 @@ void *_stdv_needs_to_grow (void *vec, size_t membsz) {
 	return vec;
 }
 
+void *_stdv_shrink (void *vec)
+{
+	_StdvHeader *header = _STDV_GET_STDVHEADER(vec);
+	const int64_t diff = header->cap - header->len;
+
+	if (diff <= 0) {
+		return vec;
+	}
+
+	header->cap = header->len;
+	free(vec + header->len);
+	return vec;
+}
 
 /*
  *  ___________________________
@@ -55,9 +74,24 @@ void *_stdv_needs_to_grow (void *vec, size_t membsz) {
  *                 ||----w |
  *                 ||     ||
  */
-#define stdv_put(vec, x)  ((vec) = _stdv_needs_to_grow(vec, sizeof(*vec)), (vec)[_STDV_GET_STDVHEADER(vec)->len++] = x)
-#define stdv_len_u64(vec) ((uint64_t) _STDV_GET_STDVHEADER(vec)->len)
-#define stdv_cap_u64(vec) ((uint64_t) _STDV_GET_STDVHEADER(vec)->cap)
-#define stdv_pop(vec)     ((vec)[--_STDV_GET_STDVHEADER(vec)->len])
+#define stdv_put(vec, x)     ((vec) = _stdv_needs_to_grow(vec, sizeof(*vec)), (vec)[_STDV_GET_STDVHEADER(vec)->len++] = x)
+
+#define stdv_is_empty(vec)   ((vec) ? (_STDV_GET_STDVHEADER(vec)->len == 0) : 0)
+#define stdv_len_u64(vec)    ((uint64_t) _STDV_GET_STDVHEADER(vec)->len)
+#define stdv_cap_u64(vec)    ((uint64_t) _STDV_GET_STDVHEADER(vec)->cap)
+#define stdv_shrink(vec)     ((vec) = _stdv_shrink(vec))
+
+#define stdv_get(vec, at)    ((vec)[at])
+#define stdv_get_or(vec, or)
+#define stdv_beg(vec)        (*(vec))
+#define stdv_end(vec)        ((vec)[_STDV_GET_STDVHEADER(vec)->len - 1])
+#define stdv_ptr_beg(vec)    (&(*(vec)))
+#define stdv_ptr_end(vec)    (&(vec)[_STDV_GET_STDVHEADER(vec)->len - 1])
+#define stdv_ptr_at(vec, at) (&(vec)[at])
+
+#define stdv_pop(vec)        ((vec)[--_STDV_GET_STDVHEADER(vec)->len])
+
+#define stdv_make_imut(vec)
+
 
 #endif
