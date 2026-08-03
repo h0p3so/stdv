127e019e871642e85961431922a751b239594435
h0p3so <juan.dpmz05@gmail.com>
2026-08-03 19:50:00 +0000

    ok

--- demo.c
+++ demo.c
@@ -3,18 +3,26 @@
 
 int main ()
 {
-	int *numbers = NULL;
+	int *numbers = stdv_create(sizeof(int), 64);
+
 	stdv_put(numbers, 0);
+	stdv_put(numbers, 1);
+	stdv_put(numbers, 2);
+	stdv_put(numbers, 3);
+	stdv_put(numbers, 4);
+	stdv_put(numbers, 5);
+	stdv_put(numbers, 6);
+	stdv_put(numbers, 7);
+	stdv_put(numbers, 8);
+	stdv_put(numbers, 9);
 
-	printf("%ld\n", stdv_capacity(numbers));
-	stdv_reserve(numbers, 32);
-	printf("%ld\n", stdv_capacity(numbers));
-	stdv_reserve(numbers, 65);
-	printf("%ld\n", stdv_capacity(numbers));
+	for (size_t i = 0; i < stdv_size(numbers); i++)
+		printf("%ld) %d\n", i + 1,numbers[i]);
+	puts("----");
 
-	stdv_shrink(numbers);
-	printf("%ld %p\n", stdv_capacity(numbers), numbers);
+	stdv_insert(numbers, 2, 69);
+	for (size_t i = 0; i < stdv_size(numbers); i++)
+		printf("%ld) %d\n", i + 1, numbers[i]);
 
-	stdv_free(numbers);
 	return 0;
 }
