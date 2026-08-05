da47296a955e20e58884808d27bfc08cd82892ae
h0p3so <juan.dpmz05@gmail.com>
2026-08-05 17:00:00 +0000

    bugs solved

--- demo.c
+++ demo.c
@@ -1,28 +1,29 @@
 #include "stdv.h"
 #include <stdio.h>
 
+void info (int *numbers)
+{
+	printf("cap: %ld\n", stdv_capacity(numbers));
+	printf("len: %ld\n", stdv_size(numbers));
+	for (size_t i = 0; i < stdv_size(numbers); i++)
+	{
+		printf("%ld) %d\n", i, numbers[i]);
+	}
+	printf("---------------\n");
+}
+
+
 int main ()
 {
-	int *numbers = stdv_create(sizeof(int), 64);
+	int *numbers = stdv_create(sizeof(int), 3);
 
 	stdv_put(numbers, 0);
-	stdv_put(numbers, 1);
-	stdv_put(numbers, 2);
-	stdv_put(numbers, 3);
-	stdv_put(numbers, 4);
+	info(numbers);
+
 	stdv_put(numbers, 5);
-	stdv_put(numbers, 6);
-	stdv_put(numbers, 7);
-	stdv_put(numbers, 8);
-	stdv_put(numbers, 9);
+	info(numbers);
 
-	for (size_t i = 0; i < stdv_size(numbers); i++)
-		printf("%ld) %d\n", i + 1,numbers[i]);
-	puts("----");
-
-	stdv_insert(numbers, 2, 69);
-	for (size_t i = 0; i < stdv_size(numbers); i++)
-		printf("%ld) %d\n", i + 1, numbers[i]);
-
+	stdv_insert(numbers, STDV_POS_OF_THE_LAST(numbers, 1), 69);
+	info(numbers);
 	return 0;
 }
